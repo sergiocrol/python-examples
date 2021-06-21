2e80aacb70ff6750ac1962782e5a712f716d9506
sergiocrol <sergio.crol@gmail.com>
2021-06-21 19:34:42 +0200

    Python examples. Comments change

--- smsProject/sms.py
+++ smsProject/sms.py
@@ -9,6 +9,7 @@ account_sid = 'ACc7464de0a39c708ec77e4d639f4ed642'
 auth_token = os.environ.get('AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
+# d
 message = client.messages.create(
     messaging_service_sid='MGd7a1a3d7400c56fa6c165060ee42c20b',
     body='Sending from codeeeeee',
